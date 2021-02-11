3786cc90ce1c6c62025bff350508aecb7c43cdcc
alicefren <78852401+alicefren@users.noreply.github.com>
2021-02-11 14:35:21 +0000

    Testing icon updates in JS

--- script.js
+++ script.js
@@ -32,6 +32,11 @@ let currentHour = todaysDate.getHours();
 let currentMinute = todaysDate.getMinutes();
 
 let liveDate = document.querySelector(".card-title");
+if (currentHour < 10) {
+  liveDate.innerHTML = `${currentDay} ${currentDate} ${currentMonth}, 0${currentHour}:${currentMinute}`;
+} else {
+  liveDate.innerHTML = `${currentDay} ${currentDate} ${currentMonth}, ${currentHour}:${currentMinute}`;
+}
 if (currentMinute < 10) {
   liveDate.innerHTML = `${currentDay} ${currentDate} ${currentMonth}, ${currentHour}:0${currentMinute}`;
 } else {
@@ -46,7 +51,25 @@ function showWeather(response) {
   document.querySelector("#max-temp").innerHTML = `High ${Math.round(response.data.main.temp_max)} °C`;
   document.querySelector("#min-temp").innerHTML = `Low ${Math.round(response.data.main.temp_min)} °C`;
   document.querySelector("#description").innerHTML = response.data.weather[0].description;
+  weatherIcon.setAttribute("#todays-icon"); 
 }
+
+
+/*function getWeatherIcon(icon) {
+  if (icon === "01d" || icon === "01n") {
+    return `<i class="fas fa-cloud-rain rain"></i>`;
+  } else if (icon === "02d" || icon === "02n" || icon === "03d" || icon === "03n" || icon === "04d" || icon === "04n") {
+    return `<i class="fas fa-cloud"></i>`;
+  } else if (icon === "09d" || icon === "09n" || icon ==="10d" ||icon === "10n") {
+    return `<i class="fas fa-cloud-showers-heavy"></i>`;
+  } else if (icon === "11d" || icon === "11n") {
+    return `<i class="fas fa-bolt"></i>`;
+  } else if (icon === "13d" || icon === "13n") {
+    return `<i class="fas fa-snowflake"></i>`;
+  } else {
+  return `<i class="fas fa-water"></i>` }
+  }*/
+
 
 function searchCity(city) {
   let apiKey = "5c043941096cfca1b8129a71701e2dcf";
@@ -84,5 +107,8 @@ function getCurrentLocation(event) {
 
 let currentLocationButton = document.querySelector("#compass");
 currentLocationButton.addEventListener("click", getCurrentLocation);
+
+
+
 
 searchCity("London");
